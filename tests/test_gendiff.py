c3eb2d33d86b8ec7ecf718e5d1e5c46607596be5
Viktor Shcherbakov <evil.logitech@gmail.com>
2023-03-09 23:42:06 +0530

    fixed test

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -2,7 +2,7 @@ from gendiff.scripts.gendiff import generate_diff
 
 
 def test_plain_diff():
-    diff = generate_diff('file1.json', 'file2.json')
+    diff = generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json')
     with open('tests/fixtures/plain_check_string') as f:
         test_string = f.read()
     assert diff == test_string
